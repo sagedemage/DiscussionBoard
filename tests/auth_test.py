49ae26a8159773af8120c96ccb625678e239cc84
Salmaan Saeed <salsaeed999@gmail.com>
2022-05-12 01:52:29 -0400

    corrected this unit test

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -132,10 +132,3 @@ def test_allow_access_to_dashboard(client):
     response = client.post("/login", data={"email": "test1000@gmail.com",
                                               "password": "test1000", "confirm": "test1000"})
     assert response.headers["Location"] == "/dashboard"
-
-
-def test_deny_access_to_discussion(client):
-    """ Deny Access to the Discussion board """
-    # denying access to the dashboard for users not logged
-    response = client.get("/discussions", follow_redirects=True)
-    assert b"Please log in to access this page." in response.data
--- tests/discussion_test.py
+++ tests/discussion_test.py
@@ -3,5 +3,13 @@
 
 def test_discussion_request(client):
     """ Test discussion page response """
-    response = client.get("/discussion")
-    assert response.status_code == 404
+    response = client.get("/discussions")
+    assert response.status_code == 302
+
+
+def test_deny_access_to_discussion(client):
+    """ Deny Access to the Discussion board """
+    # denying access to the dashboard for users not logged
+    response = client.get("/discussions", follow_redirects=True)
+    assert b"Please log in to access this page." in response.data
+
